dea0f4dd5084d137e8c50c9b874397184b4c6242
Zakary Boven <zakaryboven@gmail.com>
2022-08-29 15:28:56 +1200

    Users are presented with a list of classes to choose from

---
+++ DisabilityServiceTools/Client/Pages/Characters/ClassesCard.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using CampaignsWithoutNumber.Shared.DataTransferObjects;
+using MudBlazor;
+
+namespace CampaignsWithoutNumber.Client.Pages.Characters
+{
+  /// <summary>
+  /// Allows the user to select character classes from a list.
+  /// </summary>
+  public partial class ClassesCard
+  {
+    private IEnumerable<CharacterClassDto> _characterClasses = new List<CharacterClassDto>();
+
+    private CharacterClassDto _value;
+
+    private HashSet<CharacterClassDto> _selectedValues;
+
+    private Func<CharacterClassDto,string> _converter = p => p?.Name;
+    
+    protected override async Task OnInitializedAsync()
+    {
+      _characterClasses = await HttpClient.GetFromJsonAsync<List<CharacterClassDto>>("api/characterclass/index");
+    }
+  }
+}
