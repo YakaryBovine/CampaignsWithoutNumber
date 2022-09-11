2b454925de69a7088c6b03499e9ed2c36c885a01
Zakary Boven <zakary.boven@datacom.co.nz>
2022-09-11 17:34:13 +1200

    Documentation and reformat

--- DisabilityServiceTools/Shared/ICharacterClass.cs
+++ DisabilityServiceTools/Shared/ICharacterClass.cs
@@ -4,28 +4,42 @@ using CampaignsWithoutNumber.Shared.Entities;
 
 namespace CampaignsWithoutNumber.Shared
 {
-  public interface ICharacterClass
-  {
+	public interface ICharacterClass
+	{
+    /// <summary>
+    ///    A user-friendly name for the class.
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    ///    How much attack bonus per level this class gives.
+    ///    Only the highest value of any class is respected.
+    /// </summary>
     public float AttackBonusPerLevel { get; }
 
+    /// <summary>
+    ///    How many hit points per level this class gives.
+    ///    Only the highest value of any class is respected.
+    /// </summary>
     public float HitPointsPerLevel { get; }
 
-    public List<CharacterFeature> Features { get; }
+		public List<CharacterFeature> Features { get; }
 
     /// <summary>
-    /// Class-specific Arts that a player can select if they have this class.
+    ///    Class-specific Arts that a player can select if they have this class.
     /// </summary>
     public List<IArt> Arts { get; }
-    
+
+    /// <summary>
+    ///    Unique identifier.
+    /// </summary>
     public int Id { get; }
 
-    public void Apply(CharacterDto characterDto)
-    {
-      characterDto.HitPoints = (int)(HitPointsPerLevel * characterDto.Level);
-      characterDto.AttackBonus = (int)(AttackBonusPerLevel * characterDto.Level);
-      foreach (var feature in Features) feature.Apply(characterDto);
-    }
-  }
+		public void Apply(CharacterDto characterDto)
+		{
+			characterDto.HitPoints = (int) (HitPointsPerLevel * characterDto.Level);
+			characterDto.AttackBonus = (int) (AttackBonusPerLevel * characterDto.Level);
+			foreach (var feature in Features) feature.Apply(characterDto);
+		}
+	}
 }
