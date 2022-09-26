dcec6c5191ae7105e1e42cfb2b7dbc8b7f3a4c1c
Zakary Boven <zakaryboven@gmail.com>
2022-09-26 18:08:33 +1300

    Got some kind of map working

---
+++ DisabilityServiceTools/Server/Controllers/WorldMapController.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampaignsWithoutNumber.Server.Controllers;
+
+[Route("api/worldmap")]
+public class WorldMapController
+{
+  [HttpGet]
+  [Route("get")]
+  public object? Get()
+  {
+    var fullPath = Path.GetFullPath("map.geojson");
+    var contents = File.ReadAllText(fullPath);
+    return Newtonsoft.Json.JsonConvert.DeserializeObject<object>(contents);
+  }
+}
