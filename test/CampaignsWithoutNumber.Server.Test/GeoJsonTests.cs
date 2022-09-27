1c9c5e636677c76c8ff6902f092568134664d745
Zakary Boven <zakaryboven@gmail.com>
2022-09-27 13:03:56 +1300

    Implemented test project for Server

---
+++ test/CampaignsWithoutNumber.Server.Test/GeoJsonTests.cs
@@ -0,0 +1,19 @@
+using System.Text.Json;
+using FluentAssertions;
+using GeoJSON.Text.Feature;
+
+namespace CampaignsWithoutNumber.Server.Test;
+
+public sealed class GeoJsonTests
+{
+  [Theory]
+  [InlineData("markers.geojson")]
+  [InlineData("map.geojson")]
+  public void JsonSerializer_Deserialize_CanDeserializeStates(string filePath)
+  {
+    var text = File.ReadAllText(filePath);
+    var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(text);
+    featureCollection.Should().NotBeNull();
+    featureCollection?.Features.Count.Should().NotBe(0);
+  }
+}
