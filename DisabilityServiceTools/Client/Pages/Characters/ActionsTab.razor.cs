2eef75fbdd509613b7888cf60437860fa748ef9b
Zakary Boven <zakaryboven@gmail.com>
2025-03-28 19:36:51 +1300

    Adopt new project structure using .NET exclusively for backend (#12)

--- DisabilityServiceTools/Client/Pages/Characters/ActionsTab.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using CampaignsWithoutNumber.Shared.DataTransferObjects;
-using Microsoft.AspNetCore.Components;
-
-namespace CampaignsWithoutNumber.Client.Pages.Characters;
-
-public partial class ActionsTab
-{
-	[Parameter] public CharacterDto Character { get; set; }
-}
